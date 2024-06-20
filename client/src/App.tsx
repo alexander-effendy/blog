1c100de7ac4326b107e1db4517ed7385fc59569d
alexander-effendy <88591194+alexander-effendy@users.noreply.github.com>
2024-06-20 17:03:07 +1000

    setup: setup react vite and tailwindcss done

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="w-screen h-screen bg-slate-500 text-5xl font-bold grid place-items-center">Yo</div>
+    </>
+  )
+}
+
+export default App
