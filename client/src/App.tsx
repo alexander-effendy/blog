815fb4431bade1f252f0cf20083575d73b6de6ea
alexander-effendy <88591194+alexander-effendy@users.noreply.github.com>
2024-06-20 17:21:35 +1000

    dev: boilerplate code for rest api ajax on client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,45 @@
-import { useState } from 'react'
+import { useMediaQuery } from '@mui/material';
+import _default from '@mui/material/styles/identifier';
+import { useState, useEffect } from 'react';
+import { fetchPostsAPI, addPostAPI } from './api/PostAPI';
 import './index.css'
 
+const SideBar = () => {
+  const smallPage = useMediaQuery('(max-width:900px');
+
+  return (
+    <div className={`${smallPage ? 'hidden' : 'fixed w-[300px]'}`}>
+      <div className="font-bold text-2xl">Blog Post</div>
+      <div className="">Made by Alex :)</div>
+    </div>
+  )
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
+  const [editedId, setEditedId] = useState(-1);
+  const [newTitle, setNewTitle] = useState('');
+  const [newBody, setNewBody] = useState('');
+
+  const [refresh, setRefresh] = useState(false);
+
+  const loadData = async () => {
+    try {
+      const posts = await fetchPostsAPI();
+      setData(posts);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  const addPost = async (newPost) => {
+    try {
+      await addPostAPI(newPost);
+    } catch (error) {
+      throw new Error(error);
+    }
+    setRefresh(!refresh);
+  }
 
   return (
     <>
---
+++ client/src/api/PostAPI.tsx
@@ -0,0 +1,28 @@
+export const fetchPostAPI = async () => {
+  try {
+    const response = await fetch('http://127.0.0.1:8000/api/posts/')
+    if (!response.ok) {
+      throw new Error('Network res is not ok')
+    }
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    throw new Error('something is wrong!');
+  }
+}
+
+export const addPostAPI = async (newPost) => {
+  try {
+    const response = await fetch('http://127.0.0.1:8000/api/posts/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newPost),
+    });
+    if (!response.ok) console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+  
+}
